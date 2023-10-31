d674c69bdc3c8ee0ba1e45a715a794adeda1f4b1
abyssone <spenex@gmail.com>
2023-10-31 17:11:57 +0300

    feat(entity): добавил связь Employee - Contract

--- src/main/java/ru/abyssone/employeeworktime/entity/Employee.java
+++ src/main/java/ru/abyssone/employeeworktime/entity/Employee.java
@@ -1,10 +1,53 @@
 package ru.abyssone.employeeworktime.entity;
 
-import java.util.List;
-import java.util.Optional;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+import java.util.UUID;
+
+@Entity
+@Table(name = "employees")
+@Getter
+@Setter
+@NoArgsConstructor
 public class Employee {
+
+    /*
+     * UUID используется для корректной работы equals и hashCode вновь созданных объектов,
+     * еще не сохраненных в бд
+     * */
+    @Id
+    private UUID id = UUID.randomUUID();
+
     private String name;
-    private List<Contract> contracts;
-    private Optional<Account> account;
+
+    @OneToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL, mappedBy = "employee")
+    private Set<Contract> contracts = new HashSet<>();
+    //private Optional<Account> account;
+
+    public void addContract(Contract contract) {
+        if (this.contracts.contains(contract)) {
+            return;
+        }
+        this.contracts.add(contract);
+        contract.setEmployee(this);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Employee employee = (Employee) o;
+        return Objects.equals(id, employee.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
 }
